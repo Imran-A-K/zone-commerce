f122fb77073dd85a765a5b00eb8726473f5449e9
Imran-A-K <imran.k.professional@gmail.com>
2024-05-07 20:02:23 +0600

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import { Metadata } from "next";
+import { Recursive } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
+import Providers from "@/components/Providers";
+import { constructMetadata } from "@/lib/utils";
+
+const recursive = Recursive({ subsets: ["latin"] });
+
+export const metadata = constructMetadata();
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={recursive.className}>
+        <Navbar />
+
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            <Providers>{children}</Providers>
+          </div>
+          <Footer />
+        </main>
+
+        <Toaster />
+      </body>
+    </html>
+  );
+}
