fd29299e7d8dba8da1e122b4587e747a32e6fead
Imran-A-K <imran.k.professional@gmail.com>
2024-05-09 16:48:06 +0600

    Added related products at single products page

--- app/products/[product-id]/page.jsx
+++ app/products/[product-id]/page.jsx
@@ -2,7 +2,8 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import ImageGallery from "@/components/Product/ImageGallery";
 import Info from "@/components/Product/Info";
-import { getProduct } from "@/lib/actions/actions";
+import ProductCard from "@/components/Product/ProductCard";
+import { getProduct, getRelatedProduct } from "@/lib/actions/actions";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
@@ -11,13 +12,18 @@ function Page({ params }) {
     queryKey: ["products", params["product-id"]],
     queryFn: () => getProduct({ id: params["product-id"] }),
   });
+  const { data: relatedProducts = {}, isLoading: isRelatedProductsLoading } =
+    useQuery({
+      queryKey: ["related-products", params["product-id"]],
+      queryFn: () => getRelatedProduct({ id: params["product-id"] }),
+    });
   const availableColors = data?.variants
     ? [...new Set(data?.variants?.map((variant) => variant.code))]
     : [];
   const availableSizes = data?.variants
     ? [...new Set(data?.variants.map((variant) => variant.size))]
     : [];
-
+  console.log("related", relatedProducts);
   return (
     <MaxWidthWrapper>
       {isLoading ? (
@@ -30,6 +36,16 @@ function Page({ params }) {
             availableColors={availableColors}
             availableSizes={availableSizes}
           />
+        </div>
+      )}
+      {!isRelatedProductsLoading && relatedProducts?.products.length > 0 && (
+        <div className="w-full flex flex-wrap gap-16">
+          <p className="w-full text-2xl font-semibold pb-2 border-b-[1px] border-slate-500">
+            Related Products
+          </p>
+          {relatedProducts?.products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </div>
       )}
     </MaxWidthWrapper>
